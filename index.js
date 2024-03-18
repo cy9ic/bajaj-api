81e845a5dc91ca63ad45c254c84c58eb3f410a68
Harkaran <73885358+cy9ic@users.noreply.github.com>
2024-03-18 13:16:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
-const express = require("express");
-const usersRouter = require("./routes/users");
-const app = express();
-const port = 3000;
+import express from 'express';
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app = express();
+
+app.get('/', async (req, res) => {
+  try{
+    res.send('Hello World!');
+  }catch(error){
+    res.status(401).json({error:error})
+  }
 });
 
-app.use("/users", usersRouter);
+const emailId = "harkara1709.be21@chitkara.edu.in";
+const roll = "2110991709";
+const even = [];
+const user_id = "Harkaran_Singh_19102003";
+const odd = [];
+const alphabets  = [];
 
 
 app.post('/bfhl' , async (req , res)=>{
-  try{
-    const odd = [];
-    const alphabets  = [];
-    const even = [];
-    
-    const data = req.body.data;
-    res.status(201).json({message:data});
-    if(!data){
-      return res.status(404).json({ message: 'Data not found!' });
-    }
+  const data = req.body.data;
+   try{
+     if(!data){
+       return res.status(404).json({ message: 'Data not found!' });
+     }
+     const odd = [];
+     const alphabets  = [];
+     const even = [];
+     
+ 
+     res.status(201).json({message:data});
 
-    for(const item of data){
-      if(typeof Number(item) == "number"){
-        const num = Number(item);
-        if(num % 2 == 0){
-          even.push(num);
-        }else{
-          odd.push(num);
-        }
-      }else{
-        alphabets.push(item);
-      }
-    }  
+     for(const item of data){
+       if(typeof Number(item) == "number"){
+         const num = Number(item);
+         if(num % 2 == 0){
+           even.push(num);
+         }else{
+           odd.push(num);
+         }
+       }else{
+         alphabets.push(item);
+       }
+     }  
 
 
-    const response = {
-      "is_success": true,
-      "user_id": user_id,
-      "email":emailId,
-      "roll_number":roll,
-      "odd_numbers": odd,
-      "even_numbers": even,
-      "alphabets": alphabets
-    } 
-      res.json(response);
+     const response = {
+       "is_success": true,
+       "user_id": user_id,
+       "email":emailId,
+       "roll_number":roll,
+       "odd_numbers": odd,
+       "even_numbers": even,
+       "alphabets": alphabets
+     } 
+       res.json(response);
 
-    
-  }catch(error){
-    res.status(501).json({message:"An error has occured!" , errorMsg:error});
-  } 
+     
+   }catch(error){
+     res.status(501).json({message:"An error has occured!" , errorMsg:error});
+   } 
 }
 );
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(3000, () => {
+  console.log('Express server working');
 });
